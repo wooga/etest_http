1b8bd2725b8a0ad3ef3c66f8db0ffc102e6b55c6
Johannes Huning <mail@johanneshuning.com>
2012-04-25 18:07:56 +0200

    Sketch macros to be implemented

---
+++ include/bi_http.hrl
@@ -0,0 +1,23 @@
+-ifndef(EUNIT_HTTP_HRL).
+-define(EUNIT_HTTP_HRL, true).
+
+% % Performing HTTP Requests
+% performGet()
+% performPost()
+% performRequest()
+
+% % Both Responses and Requests
+% assertContains()
+% assertBody()
+% assertHeader()
+% assertHeaderVal()
+
+% % HTTP Responses
+% assertStatus()
+
+% % JSON specific matchers
+% assertJson()
+% assertJsonKey()
+% assertJsonVal()
+
+-endif.
